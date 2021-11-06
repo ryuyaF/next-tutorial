f5a65048033e8390805bf65b197ae73c04c65dce
ryuyaF <r.fukushima403@gmail.com>
2021-11-06 12:09:07 +0900

    アポストロフィをエスケープ文字に変更

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to ryuyaF's next-tutorial App!
+          Welcome to ryuyaF&apos;s next-tutorial App!
         </h1>
 
         <p className={styles.description}>
