85784e6f4682cd2311f2738d93b99051fc6827f1
ryuyaF <r.fukushima403@gmail.com>
2021-11-13 13:59:10 +0900

    ブログアプリを実装

---
+++ postcss.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    purge: [
+        './pages/**/*.js',
+        './components/**/*.js'
+    ],
+    plugins: [
+        'tailwindcss',
+        'postcss-flexbugs-fixes',
+        [
+            'postcss-preset-env',
+            {
+                autoprefixer: {
+                    flexbox: 'no-2009'
+                },
+                stage: 3,
+                features: {
+                    'custom-properties': false
+                }
+            }
+        ]
+    ]
+}
